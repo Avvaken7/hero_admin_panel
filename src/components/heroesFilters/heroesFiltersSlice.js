ffef51880331c79cf3e3e303c5c97c7b5b122dd6
Slava <avvaken7@gmail.com>
2023-08-27 18:11:54 +0300

    deploy to github

---
+++ src/components/heroesFilters/heroesFiltersSlice.js
@@ -0,0 +1,57 @@
+import { createSlice, createAsyncThunk, createEntityAdapter } from "@reduxjs/toolkit";
+import { useHttp } from '../../hooks/http.hook';
+
+
+const filtersAdapter = createEntityAdapter();
+
+const initialState = filtersAdapter.getInitialState({
+    filtersLoadingStatus: 'idle',
+    activeFilter: 'all'
+});
+
+// const initialState = {
+//     filters: [],
+//     filtersLoadingStatus: 'idle',
+//     activeFilter: 'all'
+// }
+
+export const filteredHeroes = createAsyncThunk(
+    'filters/filteredHeroes',
+    async () => {
+        const { request } = useHttp();
+        return await request('http://localhost:3001/filters');
+    }
+)
+
+const heroesFiltersSlice = createSlice({
+    name: 'filters',
+    initialState,
+    reducers: {
+        activeFilterChanged: (state, action) => {
+            state.activeFilter = action.payload;
+        }
+    },
+    extraReducers: (builder) => {
+        builder
+        .addCase(filteredHeroes.pending, state => { state.filtersLoadingStatus = 'loading' })
+        .addCase(filteredHeroes.fulfilled, (state, action) => {
+            state.filtersLoadingStatus = 'idle';
+            filtersAdapter.setAll(state, action.payload);
+        })
+        .addCase(filteredHeroes.rejected, state => { state.filtersLoadingStatus = 'error' })
+        .addDefaultCase(() => {});
+    }
+})
+
+const { actions, reducer } = heroesFiltersSlice;
+
+export default reducer;
+
+export const { selectAll } = filtersAdapter.getSelectors(state => state.filters);
+
+export const {
+    filtersFetching,
+    filtersFetched,
+    filtersFetchingError,
+    activeFilterChanged
+} = actions;
