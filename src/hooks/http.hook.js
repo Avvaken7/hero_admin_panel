fedd7c753bdb4b3243e3bac6fdd5c2356f314c4b
Slava <avvaken7@gmail.com>
2023-08-30 22:10:08 +0300

    fix custom httpHook

--- src/hooks/http.hook.js
+++ src/hooks/http.hook.js
@@ -1,20 +1,25 @@
 export const useHttp = () => {
-
-    const request = async (url, method = 'GET', body = null, headers = { 'Content-Type': 'application/json' }) => {
-        try {
-            const response = await fetch(url, { method, body, headers });
-
-            if (!response.ok) {
-                throw new Error(`Could not fetch ${url}, status: ${response.status}`);
-            }
-
-            const data = await response.json();
-
-            return data;
-        } catch (e) {
-            throw e;
+    const request = async (
+      url,
+      target = '',
+      method = 'GET',
+      body = null,
+      headers = { 'Content-Type': 'application/json' },
+    ) => {
+      try {
+        const response = await fetch(url, { method, body, headers });
+  
+        if (!response.ok) {
+          throw new Error(`Could not fetch ${url}, status: ${response.status}`);
         }
-    }
-
-    return { request }
-}
+  
+        const data = await response.json();
+  
+        return target ? data[target] : data;
+      } catch (e) {
+        throw e;
+      }
+    };
+  
+    return { request };
+  };
